ae9ba985647fe6e566724a2b1078a19e4295670d
stevecallender <mahogy@hotmail.co.uk>
2017-10-24 21:09:33 +0100

    UPdated network for new ip addresses

--- launch_home.sh
+++ launch_home.sh
@@ -10,13 +10,14 @@
 #home control
 python python_workspace/home_control_backend_py/TimeMonitor.py &
 sleep 10
-python python_workspace/home_control_backend_py/PlugDriver.py &
-sleep 10
+#python python_workspace/home_control_backend_py/PlugDriver.py &
+#sleep 10
 python python_workspace/home_control_backend_py/HomeController.py &
 sleep 10
 python python_workspace/home_control_backend_py/NetworkMonitor.py &
 
 #node
-#cd HAP-NodeJS
-#sudo rm -rf perist
-#sudo node Core.js
+sleep 10
+cd HAP-NodeJS
+sudo rm -rf perist
+sudo node Core.js
